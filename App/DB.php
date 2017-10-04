ad29b1f4a8443b5361c8c55cae34a0522c14cb8d
Andrey Erdikov <andystory@mail.ru>
2017-10-04 13:18:05 +0700

    !Добавлена автозагрузка

---
+++ App/DB.php
@@ -0,0 +1,18 @@
+<?php
+    namespace App;
+/**
+ * Created by PhpStorm.
+ * User: Andrey
+ * Date: 04.10.2017
+ * Time: 10:30
+ */
+    class DB
+    {
+
+        function __construct()
+        {
+            echo 'Hello, DB!';
+        }
+
+
+    }
